85588658b8d6048bd7873a6efba880ce9eb6be12
rahul <nizarerahul7@gmail.com>
2024-10-05 22:26:36 +0530

    nav of mobile

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const setting = document.querySelector(".setting");
 const navRight = document.querySelector(".nav-right");
+const faReg = document.querySelector(".fa-regular")
+let isVisible = false;
  setting.addEventListener("click",()=>{
-    navRight.style.transform = "translateX(0)"
-    setting.style.left = "80%"
-    setting.onclick = function(){
-        setting.style.left = "-20%"
-        navRight.style.transform = "translate(100%)"
-        return;
+    if(!isVisible){
+        navRight.style.transform = "translateX(0)"
+        setting.style.left = "80%"
     }
-    return;
+    else{
+        navRight.style.transform = "translateX(100%)";
+        setting.style.left = "-20%"
+    }
+    isVisible = !isVisible;
+ })
+ faReg.addEventListener("click",()=>{
+    if(faReg.classList.contains("fa-moon")){
+        faReg.classList.remove("fa-moon");
+        faReg.classList.add("fa-sun");
+    }
+    else{
+        faReg.classList.remove("fa-sun")
+        faReg.classList.add("fa-moon")
+    }
+    faReg.classList.add("animate")
+    setTimeout(() => {
+        faReg.classList.remove("animate")
+    }, 500);
  })
